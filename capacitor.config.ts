a3e3cf3b9e8d74dd43d71354f43436df213d3819
Ram <ramkumar@vthink.co.in>
2023-07-14 20:46:32 +0530

    Base Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'meeting_room_schedule',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
