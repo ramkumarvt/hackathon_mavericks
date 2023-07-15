50e79a1ab64d6567463c471e297189501225a3c3
logeswaranV <logeswaran@vthink.co.in>
2023-07-15 11:49:02 +0530

    room-status

--- src/app/pages/app-room-status/app-room-status.module.ts
+++ src/app/pages/app-room-status/app-room-status.module.ts
@@ -7,12 +7,14 @@ import { IonicModule } from '@ionic/angular';
 import { AppRoomStatusPageRoutingModule } from './app-room-status-routing.module';
 
 import { AppRoomStatusPage } from './app-room-status.page';
+import { SharedModule } from 'src/app/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
+    SharedModule,
     AppRoomStatusPageRoutingModule
   ],
   declarations: [AppRoomStatusPage]
